90b4b3205beef5ce8bd9d0d94262911ee3053432
c0b23077000 <c0b2307700@edu.teu.ac.jp>
2024-09-24 15:20:47 +0900

    練習1,3: 背景画像の描画

---
+++ pygame_image.py
@@ -0,0 +1,28 @@
+import os
+import sys
+import pygame as pg
+
+os.chdir(os.path.dirname(os.path.abspath(__file__)))
+
+
+def main():
+    pg.display.set_caption("はばたけ！こうかとん") # ゲームのタイトル部分
+    screen = pg.display.set_mode((800, 600)) # 800x600のサイズをタプルで渡す
+    clock  = pg.time.Clock()
+    bg_img = pg.image.load("fig/pg_bg.jpg") # load()で画像読み込む
+    tmr = 0
+    while True:
+        for event in pg.event.get():
+            if event.type == pg.QUIT: return
+
+        screen.blit(bg_img, [0, 0])
+        pg.display.update()
+        tmr += 1        
+        clock.tick(10)
+
+
+if __name__ == "__main__":
+    pg.init()
+    main()
+    pg.quit()
+    sys.exit()
