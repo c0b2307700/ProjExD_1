b90ae4e0c6dd1e3c42ce6a88fc1bc882bd35358e
c0b23077000 <c0b2307700@edu.teu.ac.jp>
2024-09-24 16:25:11 +0900

    練習8: こうかとんを動かせるようにする！

--- pygame_image.py
+++ pygame_image.py
@@ -13,16 +13,29 @@ def main():
     bg_img1 = pg.transform.flip(bg_img, True, False) # 練習7反転背景
     kk_img = pg.image.load("fig/3.png") # 練習2
     kk_img = pg.transform.flip(kk_img, True, False) # 練習2 反転 (画像, 左右, 上下)
+    kk_rct = kk_img.get_rect() # 練習8-1 sarfaceからrectを抽出する
+    kk_rct.center = 300, 200 # 練習8-1 rectを用いた初期座標の設定
     tmr = 0
     while True:
-        for event in pg.event.get():
+        for event in pg.event.get(): # 2回目で説明？
             if event.type == pg.QUIT: return
+        key_lst = pg.key.get_pressed() # 練習8-3 キーの押下状態を判定
+        if key_lst[pg.K_UP]: # ↑キーが押されていたら
+            kk_rct.move_ip((0, -1)) # こうかとんのY座標を-1する
+        if key_lst[pg.K_DOWN]: # 練習8-4 あたり
+            kk_rct.move_ip((0, +1)) # move_ip(x, y)で移動させることができる
+        if key_lst[pg.K_RIGHT]:
+            kk_rct.move_ip((+1, 0))
+        if key_lst[pg.K_LEFT]:
+            kk_rct.move_ip((-1, 0))
+        
+        
         x = -(tmr%3200)
         screen.blit(bg_img, [x, 0]) # 練習6 間延びと画像動かす
         screen.blit(bg_img1, [x+1600, 0]) # 練習7 2つ目の背景
         screen.blit(bg_img, [x+3200, 0])
         screen.blit(bg_img1, [x+4800, 0])
-        screen.blit(kk_img, [300, 200]) # 練習3 貼る順番は注意する
+        screen.blit(kk_img, kk_rct) # 練習3 貼る順番は注意する -> # 練習8-5 座標を先ほど保存したものにする
         pg.display.update()
         tmr += 1
         # if tmr > 800:
